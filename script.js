e6f66b53afef175f30448371cac3612235e83d00
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-03-05 15:15:26 +0100

    [add] milestone 0, 1

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ ogni oggetto (membro) ha 3 proprietà : nome ruolo e foto
 console log di ogni oggetto 
 stampare le informazioni in output
 */
+
+const membri = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    img: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    img: "walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    img: "scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg"
+  }
+  
+]
+console.log(membri);
